54f237b59586a8b21f73de77c1310a4b2c078ace
UmairButt <umairamjadbutt1@gmail.com>
2022-11-21 22:57:19 +0500

    importing the necessary files and making the changes

--- next-js-project-marketPlace/marketplace-revamp/pages/index.js
+++ next-js-project-marketPlace/marketplace-revamp/pages/index.js
@@ -1,9 +1,37 @@
+import {
+  GetRepairdeskCookie,
+  generateNewCookie,
+} from "../store/Slices/cookieSlice";
+
+import { getVendors, getVendorsApi } from "../store/Slices/vendorSlice";
+//Local Imports
 import styles from "../styles/Home.module.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Banner from "../components/Banner";
+import VendorCards from "../components/VendorCard";
+import { AppDispatch } from "../store/index";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 export default function Home() {
+  const dispatch = useDispatch();
+  const rdCookie = useSelector((state) => state.cookie);
+
+  GetCookie(async () => {
+    document.getElementById("body");
+  });
+
+  useEffect(() => {
+    console.log(rdCookie, "Outside the is statement");
+    if (rdCookie.rdCookie != undefined && rdCookie.rdCookie != null) {
+      dispatch(getVendorsApi());
+    } else {
+      console.log("<<=======NOT FOUND THE COOKIE=======>>");
+      window.location.href = "URL";
+    }
+  }, [rdCookie]);
+
   return (
     <div className="flex flex-col min-h-screen bg-gray-100">
       <div className="mp-header">
