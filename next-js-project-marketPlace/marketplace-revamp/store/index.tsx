c47620ddf26c4a284481a9b3f67aa8a9f74241bd
UmairButt <umairamjadbutt1@gmail.com>
2022-11-21 22:54:12 +0500

    changed the index file accordingly

--- next-js-project-marketPlace/marketplace-revamp/store/index.tsx
+++ next-js-project-marketPlace/marketplace-revamp/store/index.tsx
@@ -1,10 +1,12 @@
 import CookieReducer from "./Slices/cookieSlice";
+import vendorSlice from "./Slices/vendorSlice";
 import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 // import { createWrapper } from "next-redux-wrapper";
 
 export const store = configureStore({
   reducer: {
     cookie: CookieReducer,
+    vendors: vendorSlice,
   },
 
   middleware: getDefaultMiddleware({
